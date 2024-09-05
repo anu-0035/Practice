0318f5050d9203009b90371b848e5db487d256dd
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-05 19:14:46 +0530

    Update code.py

--- lc/2028. Find Missing Observations/code.py
+++ lc/2028. Find Missing Observations/code.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        missing = (len(rolls) + n) * mean - sum(rolls)
+        if missing <  0 or missing > 6 * n :
+             return []
+        x = missing // n
+        y = missing % n
+        if x == 0:
+            return []
+        a = [x] * n
+        for i in range(y):
+            a[i] += 1
+        return a
