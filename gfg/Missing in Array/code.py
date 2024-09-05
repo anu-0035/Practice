5d10c313464c8e6cc76b81e310d0e39aba38daf2
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-05 12:35:01 +0530

    Update code.py

--- gfg/Missing in Array/code.py
+++ gfg/Missing in Array/code.py
@@ -1,1 +1,9 @@
-
+#User function Template for python3
+class Solution:
+    
+    # Note that the size of the array is n-1
+    
+    def missingNumber(self, n, arr):
+        total = sum(arr)
+        x = (n * (n+1))/2
+        return int(x - total)
