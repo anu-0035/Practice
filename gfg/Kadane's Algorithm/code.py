c039bab35cb2619f42c770fe384bb772f41ead9f
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-06 09:18:38 +0530

    Update code.py

--- gfg/Kadane's Algorithm/code.py
+++ gfg/Kadane's Algorithm/code.py
@@ -1,1 +1,13 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    #Function to find the sum of contiguous subarray with maximum sum.
+    def maxSubArraySum(self,arr):
+        ##Your code here
+        max_so_far = max_ending_here = arr[0]
+        for i in range(1,len(arr)):
+            max_ending_here = max(arr[i],arr[i]+ max_ending_here)
+            max_so_far = max(max_ending_here,max_so_far)
+        return max_so_far
 
