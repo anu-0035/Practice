5da64274b1df038639f91b36d21b7b31efb7468f
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-06 09:30:02 +0530

    Update code.py

--- lc/3217. Delete Nodes From Linked List Present in Array/code.py
+++ lc/3217. Delete Nodes From Linked List Present in Array/code.py
@@ -1,1 +1,17 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        nums = set(nums)
+        dummy = ListNode(0,head)
+        prev = dummy
+        while prev.next:
+            if prev.next.val in nums:
+                prev.next = prev.next.next
+            else:
+                prev = prev.next
+        return dummy.next
+        
