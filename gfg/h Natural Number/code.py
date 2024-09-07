1b6fccd27b454eaba6a82875ec420d4add1417fc
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-07 23:28:24 +0530

    Update code.py

--- gfg/h Natural Number/code.py
+++ gfg/h Natural Number/code.py
@@ -1,1 +1,11 @@
+#User function Template for python3
 
+class Solution:
+    def findNth(self,n):
+        #code here
+        res,base = 0,1
+        while n>0:
+            res += (n % 9) * base
+            n //= 9
+            base *= 10
+        return res
