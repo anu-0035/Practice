8ec03503d6b941fa6b7c63eb3869fdbc9b96eb9a
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-09 12:13:12 +0530

    Update code.py

--- gfg/Minimum Jumps/code.py
+++ gfg/Minimum Jumps/code.py
@@ -1,1 +1,20 @@
-
+#User function Template for python3
+class Solution:
+	def minJumps(self, arr):
+	    #code here
+	    if arr[0] == 0: return -1
+	    n = len(arr)
+	    if n == 1 : return 0
+	    max_reach,steps,jump = arr[0],arr[0],1
+	    for i in range(1,n):
+	        if i == n-1:
+	            return jump
+	        max_reach = max(max_reach,i+arr[i])
+	        steps -= 1
+	        if steps == 0:
+	            jump += 1
+	            if i >= max_reach:
+	               return -1
+	            steps = max_reach - i
+	    return -1
+	    
