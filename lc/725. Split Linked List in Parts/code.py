404e2cb137cd9c8d1d1a3822f74ae687889981c5
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-09 12:20:34 +0530

    Update code.py

--- lc/725. Split Linked List in Parts/code.py
+++ lc/725. Split Linked List in Parts/code.py
@@ -1,1 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+        ans = []
+        t1 = head
+        n = 0
+        while t1:
+            n +=1
+            t1 = t1.next
+        part,left = n//k,n%k
+        t1,prev = head,None
+        for _ in range(k):
+            ans.append(t1)
+            for _ in range(part):
+                if t1:
+                    prev = t1
+                    t1 = t1.next
+            if left and t1:
+                prev = t1
+                t1 = t1.next
+                left -= 1
+            if prev: prev.next = None
+        return ans 
+                
 
+
+        
