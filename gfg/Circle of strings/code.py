0558d335920ba851ae64b9236eaba8b1ea61a4a1
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-10 09:36:34 +0530

    Update code.py

--- gfg/Circle of strings/code.py
+++ gfg/Circle of strings/code.py
@@ -1,1 +1,31 @@
-
+#User function Template for python3
+from collections import defaultdict
+class Solution:
+    def isCircle(self, arr):
+        # code here
+        in_degree = defaultdict(int)
+        out_degree = defaultdict(int)
+        graph = defaultdict(list)
+        
+        for s in arr:
+            graph[s[0]].append(s[-1])
+            out_degree[s[-1]] += 1
+            in_degree[s[0]] += 1
+        
+        for node in set(in_degree.keys()).union(set(out_degree.keys())):
+            if in_degree[node] != out_degree[node]:
+                return 0
+        
+        visited = set() 
+        def dfs(node):
+            visited.add(node)
+            for nei in graph[node]:
+                if nei not in visited:
+                    dfs(nei)
+        dfs(arr[0][0])
+        
+        for node in set(in_degree.keys()).union(set(out_degree.keys())):
+            if node not in visited :
+                return 0
+        
+        return 1
