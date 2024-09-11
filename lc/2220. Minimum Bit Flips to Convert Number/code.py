57465f02b799a8866a4ce5c16315c7888da7a684
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-11 20:40:27 +0530

    Update code.py

--- lc/2220. Minimum Bit Flips to Convert Number/code.py
+++ lc/2220. Minimum Bit Flips to Convert Number/code.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        #return bin(start ^ goal)[2:].count("1")
+        c = 0
+        while start or goal:
+            if start%2 != goal%2:
+                c+=1
+            start = start//2
+            goal = goal//2
+        return c
