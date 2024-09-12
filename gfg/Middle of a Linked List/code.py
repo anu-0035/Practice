18b06fcc090b80c6cd82a80aec3a7beaaccbec4b
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-12 08:29:37 +0530

    Update code.py

--- gfg/Middle of a Linked List/code.py
+++ gfg/Middle of a Linked List/code.py
@@ -1,1 +1,21 @@
+# your task is to complete this function
 
+'''
+class node:
+    def __init__(data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    #  Should return data of middle node. If linked list is empty, then  -1
+    def findMid(self, head):
+        # Code here
+        # return the value stored in the middle node
+        if not head:
+            return None
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow.data
