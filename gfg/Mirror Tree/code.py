13efcd43d46d1ef20a49109e32595957aa14e88e
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-13 09:22:45 +0530

    Update code.py

--- gfg/Mirror Tree/code.py
+++ gfg/Mirror Tree/code.py
@@ -1,1 +1,19 @@
+'''User function Template for python3
 
+
+class Node:
+    def _init_(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+# your task is to complete this function
+
+class Solution:
+    #Function to convert a binary tree into its mirror tree.
+    def mirror(self,root):
+        # Code here
+        if not root: return
+        self.mirror(root.left)
+        self.mirror(root.right)
+        root.left,root.right = root.right,root.left
