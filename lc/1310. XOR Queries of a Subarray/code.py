44a0ef54fb259ed7b5561e916ab5baace238c1ed
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-13 09:14:06 +0530

    Update code.py

--- lc/1310. XOR Queries of a Subarray/code.py
+++ lc/1310. XOR Queries of a Subarray/code.py
@@ -1,1 +1,12 @@
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        n = len(arr)
+        prefixXOR = [0] * (n+1)
+        for i in range(1,n+1):
+            prefixXOR[i] = prefixXOR[i-1] ^ arr[i-1]
 
+        ans = []
+        for x,y in queries:
+            ans.append(prefixXOR[y+1] ^ prefixXOR[x])
+        return ans
+        
