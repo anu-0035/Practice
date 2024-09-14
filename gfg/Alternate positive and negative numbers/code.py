b7f985f6ce4f8a5562ea94c71fd51009b29f9831
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-14 09:36:19 +0530

    Update code.py

--- gfg/Alternate positive and negative numbers/code.py
+++ gfg/Alternate positive and negative numbers/code.py
@@ -1,1 +1,25 @@
-
+class Solution:
+    def rearrange(self, arr):
+        pos = []
+        neg = []
+        
+        # Separate positive and negative numbers
+        for i in arr:
+            if i < 0:
+                neg.append(i)
+            else:
+                pos.append(i)
+        
+        i, j = 0, 0
+        ans = []
+        
+        # Start with a positive number and then alternate
+        while i < len(pos) and j < len(neg):
+            ans.append(pos[i])
+            ans.append(neg[j])
+            i += 1
+            j += 1
+        
+        ans += pos[i:] if i < len(pos) else neg[j:]
+        
+        return ans
