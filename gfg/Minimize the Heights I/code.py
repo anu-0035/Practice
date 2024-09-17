0676529a3fb74db336300ea64f78d1db3917c348
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-17 09:22:52 +0530

    Update code.py

--- gfg/Minimize the Heights I/code.py
+++ gfg/Minimize the Heights I/code.py
@@ -1,1 +1,15 @@
+#User function Template for python3
 
+class Solution:
+    def getMinDiff(self, arr, n, k):
+        # code here
+        arr.sort()
+        diff = arr[-1] - arr[0]
+        smallest = arr[0] + k
+        largest = arr[-1] - k
+        for i in range(n-1):
+            min_ht = min(smallest,arr[i+1] - k)
+            max_ht = max(largest,arr[i] + k)
+            
+            diff = min(diff,max_ht - min_ht)
+        return diff
