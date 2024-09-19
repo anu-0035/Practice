cb687c90c63838fd7922827158e8fe239e114290
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-19 08:50:57 +0530

    Update code.py

--- lc/179. Largest Number/code.py
+++ lc/179. Largest Number/code.py
@@ -1,1 +1,17 @@
-
+from functools import cmp_to_key
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = [str(num) for num in nums]
+        
+        # Define the custom comparator function
+        def compare(x, y):
+            return -1 if x + y > y + x else 1
+        
+        # Sort the array using the custom comparator
+        nums.sort(key=cmp_to_key(compare))
+        
+        # Concatenate the sorted strings
+        largest_number = ''.join(nums)
+        
+        # Remove leading zeros (if any)
+        return largest_number.lstrip('0') or '0'
