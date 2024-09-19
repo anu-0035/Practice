4f472f8e1b84368f6a395b928592bdcd53c80eec
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-19 08:43:47 +0530

    Update code.py

--- gfg/Reverse Words/code.py
+++ gfg/Reverse Words/code.py
@@ -1,1 +1,10 @@
+# User function Template for python3
 
+class Solution:
+    
+    #Function to reverse words in a given string.
+    def reverseWords(self,s):
+        # code here 
+        a = s.split(".")
+        a.reverse()
+        return ".".join(a)
