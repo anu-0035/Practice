600ef27172f7755d6c06aaca88b4f2ddf5ed00b8
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-19 09:14:03 +0530

    Update code.py

--- lc/241. Different Ways to Add Parentheses/code.py
+++ lc/241. Different Ways to Add Parentheses/code.py
@@ -1,1 +1,20 @@
+class Solution:
+    operations = {"+": lambda x,y: x + y,
+                 "-": lambda x,y: x - y,
+                 "*": lambda x,y: x * y,
+                }
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        res = []
+        for i in range(len(expression)):
+            op = expression[i]
+            if op in self.operations:
+                num1 = self.diffWaysToCompute(expression[:i])
+                num2 = self.diffWaysToCompute(expression[i+1:])
 
+                for n1 in num1:
+                    for n2 in num2:
+                        res.append(self.operations[op](n1,n2))
+        if res == []:
+            res.append(int(expression))
+
+        return res
