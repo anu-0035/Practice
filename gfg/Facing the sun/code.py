a9ac22e7f29117dcebae9a507bf271eaabdfd963
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-20 06:59:42 +0530

    Update code.py

--- gfg/Facing the sun/code.py
+++ gfg/Facing the sun/code.py
@@ -1,1 +1,13 @@
+#User function Template for python3
 
+class Solution:
+    # Returns count buildings that can see sunlight
+    def countBuildings(self, height):
+        # code here\
+        x,c = height[0],1
+        for i in range(1,len(height)):
+            if height[i] > x:
+                x = height[i]
+                c +=1
+        return c
+            
