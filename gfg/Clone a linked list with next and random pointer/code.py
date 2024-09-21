e3ebd9cf172248e12a331edb7a0f00b71f35f6f6
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-21 10:06:46 +0530

    Update code.py

--- gfg/Clone a linked list with next and random pointer/code.py
+++ gfg/Clone a linked list with next and random pointer/code.py
@@ -1,1 +1,34 @@
+#User function Template for python3
 
+'''
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+        self.random=None
+        
+param: head:  head of linkedList to copy
+return: the head of the copied linked list the #output will be 1 if successfully copied
+'''
+class Solution:
+    #Function to clone a linked list with next and random pointer.
+    def copyList(self, head):
+        if not head :
+            return None
+        # code here
+        oldToCopy = {None : None}
+        
+        cur = head
+        while cur:
+            copy = Node(cur.data)
+            oldToCopy[cur] = copy
+            cur = cur.next
+        
+        cur = head
+        while cur:
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random  = oldToCopy[cur.random] 
+            cur = cur.next
+        
+        return oldToCopy[head]
