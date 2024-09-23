dac268c9e80db791baeef0efe7cad8db08cbe39c
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-23 12:31:27 +0530

    Update code.py

--- gfg/Missing And Repeating/code.py
+++ gfg/Missing And Repeating/code.py
@@ -1,1 +1,21 @@
+#User function Template for python3
+
+class Solution:
+    def findTwoElement( self,arr): 
+        repeated,missing,x = -1,-1,0
+        n = len(arr)
+        count = [0] * (n+1)
+        for i in arr:
+            count[i] += 1
+        for i in range(1,n+1):
+            if count[i] == 2:
+                repeated = i
+                x += 1
+            elif count[i] == 0:
+                missing = i
+                x += 1
+            if  x == 2:
+                break
+            
+        return [repeated,missing]
 
