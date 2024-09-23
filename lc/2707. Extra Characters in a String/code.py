0b3598cab18795c2e11e63868e6b8eb667284fef
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-23 12:39:59 +0530

    Update code.py

--- lc/2707. Extra Characters in a String/code.py
+++ lc/2707. Extra Characters in a String/code.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        word_list = set(dictionary)
+        n = len(s)
+        dp = [0] * (n+1)
+        dp[0] = 0
+        for i in range(1,n+1):
+            dp[i] = dp[i-1] +1
+            for j in range(i):
+                if s[j:i] in word_list:
+                    dp[i] = min(dp[i],dp[j])        
+        return dp[n]
