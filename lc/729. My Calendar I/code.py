9b9938339812475943108a54b8c195c8fb9dc529
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-26 09:11:55 +0530

    Update code.py

--- lc/729. My Calendar I/code.py
+++ lc/729. My Calendar I/code.py
@@ -1,1 +1,40 @@
+class Tree:
+    def __init__(self,start,end):
+        self.left = None
+        self.right = None
+        self.start = start
+        self.end = end
+    def insert(self,start,end):
+        curr = self
+        while True:
+            if start >= curr.end:
+                if not curr.right:
+                    curr.right = Tree(start,end)
+                    return True
+                curr = curr.right
+            elif end <= curr.start:
+                if not curr.left:
+                    curr.left = Tree(start,end)
+                    return True
+                curr = curr.left
+            else:
+                return False
 
+
+class MyCalendar:
+
+    def __init__(self):
+        self.root = None 
+        
+
+    def book(self, start: int, end: int) -> bool:
+        if not self.root:
+            self.root = Tree(start,end)
+            return  True
+        return self.root.insert(start,end)
+
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
