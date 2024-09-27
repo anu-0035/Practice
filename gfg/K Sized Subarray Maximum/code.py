f2f5ca3e401635d221b3af76c03f1c2af01f7889
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-27 10:36:52 +0530

    Update code.py

--- gfg/K Sized Subarray Maximum/code.py
+++ gfg/K Sized Subarray Maximum/code.py
@@ -1,1 +1,20 @@
-
+#User function Template for python3
+from collections import deque
+class Solution:
+    
+    #Function to find maximum of each subarray of size k.
+    def max_of_subarrays(self,k,arr):
+        ans = []
+        dq = deque()
+        for i in range(len(arr)):
+            if dq and dq[0] < i-k+1:
+                dq.popleft()
+            while dq and arr[dq[-1]] < arr[i]:
+                dq.pop()
+            dq.append(i)
+            if i >= k-1:
+                ans.append(arr[dq[0]])
+            
+        return ans
+        
+        #code here
