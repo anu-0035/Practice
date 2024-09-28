5b2b56c6a9677d7ad62ba07311fe23db5b083e20
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-28 10:22:11 +0530

    Update code.py

--- gfg/Minimal Cost/code.py
+++ gfg/Minimal Cost/code.py
@@ -1,1 +1,12 @@
-
+#User function Template for python3
+class Solution:
+    def minimizeCost(self, k, arr):
+        # code here
+        n = len(arr)
+        dp = [float('inf')] * n
+        dp[0] = 0
+        for i in range(1,n):
+            for j in range(max(0,i-k),i):
+                if i-j >= 0:
+                    dp[i] = min(dp[i],dp[j]+abs(arr[i] - arr[j] ))
+        return  dp[n-1]
