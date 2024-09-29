bf7c828a93f9be55e785063d8e985c27a67d4ad4
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-09-29 09:47:19 +0530

    Update code.py

--- gfg/Total count/code.py
+++ gfg/Total count/code.py
@@ -1,1 +1,13 @@
 
+class Solution:
+    def count(self,x,k):
+        if x % k == 0 :
+            return int(x//k)
+        return int(x//k) + 1
+    def totalCount(self, k, arr):
+        # code here
+        x = 0
+        for i in arr:
+            x += self.count(i,k)
+        return x
+            
