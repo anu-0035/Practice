adf0128006db6c38bf1de7782510566265da1078
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-01 19:28:50 +0530

    Update code.py

--- lc/1497. Check If Array Pairs Are Divisible by k/code.py
+++ lc/1497. Check If Array Pairs Are Divisible by k/code.py
@@ -1,1 +1,15 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        reminder_count = [0] * k
+        for num in arr:
+            reminder = num % k
+            if reminder < 0:
+                reminder += k
+            reminder_count[reminder] += 1
+        if reminder_count[0] % 2 != 0:
+            return False
+        for i in range(1,k//2 +1):
+            if reminder_count[i] != reminder_count[k - i]:
+                return False
+        return True
 
