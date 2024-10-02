907f673218af3d5ff08b7bba0080024a0977206b
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-02 18:41:30 +0530

    Update code.py

--- gfg/Rotate and delete/code.py
+++ gfg/Rotate and delete/code.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def rotateDelete(self,  arr):
+        # code here
+        n = len(arr)
+        for k in range(1,n//2 + 1):
+            arr = [arr[-1]] + arr[:-1]
+            del arr[-k]
+        return arr[0]
