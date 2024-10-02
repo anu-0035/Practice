7a4067a77dc47cb705c7d78251a106ecdfaea02e
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-02 18:42:11 +0530

    Update code.py

--- lc/1331. Rank Transform of an Array/code.py
+++ lc/1331. Rank Transform of an Array/code.py
@@ -1,1 +1,7 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        sorted_unique = sorted(set(arr))
+        rank_map = {val:rank+ 1 for rank,val in enumerate(sorted_unique)}
+        return [rank_map[val] for val in arr]
 
+        
