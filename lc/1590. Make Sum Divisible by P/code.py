9a41e53515ea877e28dd28cb412f2b63aa8b8cfc
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-03 19:39:30 +0530

    Update code.py

--- lc/1590. Make Sum Divisible by P/code.py
+++ lc/1590. Make Sum Divisible by P/code.py
@@ -1,1 +1,23 @@
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        x = sum(nums)
+        remain = x % p
+        res = 0
+        if remain == 0 :
+            return res
+        
+        res = len(nums)
+        cur_sum = 0
+        remain_to_idx = {0:-1}
 
+        for i,n in enumerate(nums):
+            cur_sum = (cur_sum + n) % p
+            prefix = (cur_sum - remain + p) % p
+            if prefix in remain_to_idx :
+                length = i - remain_to_idx[prefix] 
+                res = min(res,length)
+            remain_to_idx[cur_sum] = i
+        
+        return -1 if res == len(nums) else res
+        
+        
