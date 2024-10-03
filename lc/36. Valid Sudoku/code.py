d2007165b65b8b084db11af4e57af82204531f1e
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-03 19:52:26 +0530

    Update code.py

--- lc/36. Valid Sudoku/code.py
+++ lc/36. Valid Sudoku/code.py
@@ -1,1 +1,54 @@
+class Solution:
+    def isValidSudoku(board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        row = defaultdict(set)
+        sq = defaultdict(set)
 
+        for r in range(9):
+            for c in range(9):
+                cell = board[r][c]
+
+                if cell == '.':
+                    continue
+
+                if(cell in row[r] or cell in cols[c] or cell in sq[(r//3, c//3)]):
+
+                    return False
+                else:
+                    row[r].add(cell)
+                    cols[c].add(cell)
+                    sq[(r//3, c//3)].add(cell)
+
+        return True
+
+    with open('user.out', 'w') as f:
+        for case in map(loads, stdin):
+            f.write(f'{str(isValidSudoku(case)).lower()}\n')
+    exit()
+# class Solution:
+#     def isValidSudoku(self, board: List[List[str]]) -> bool:
+#         rows = [set() for _ in range(9)]
+#         cols = [set() for _ in range(9)]
+#         subgrid = [set() for _ in range(9)]
+
+#         for i in range(9):
+#             for j in range(9):
+#                 num = board[i][j] 
+#                 if num == ".":
+#                     continue
+#                 if num in rows[i]:
+#                     return False
+#                 rows[i].add(num)
+
+#                 if num in cols[j]:
+#                     return False
+#                 cols[j].add(num)
+                
+#                 idx = (i // 3) * 3 + (j // 3)
+#                 if num in subgrid[idx]:
+#                     return False
+#                 subgrid[idx].add(num)
+        
+#         return True
+
+        
