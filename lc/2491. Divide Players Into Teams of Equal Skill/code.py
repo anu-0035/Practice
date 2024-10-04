83ca85556c542ffb658786a44a62109d53aac281
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-04 11:53:32 +0530

    Update code.py

--- lc/2491. Divide Players Into Teams of Equal Skill/code.py
+++ lc/2491. Divide Players Into Teams of Equal Skill/code.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        skill.sort()
+        total_skill = skill[0] + skill[-(1)]
+        ans = 0
+        for i in range(len(skill)//2):
+            if skill[i] + skill[-(i+1)] != total_skill:
+                return -1 
+            ans += (skill[i] * skill[-(i+1)])
+        return ans
+with open("user.out", "w") as f:
+    inputs = map(loads, stdin)
+    for nums in inputs:
+        print(Solution().dividePlayers(nums),file=f)
+exit(0)        
