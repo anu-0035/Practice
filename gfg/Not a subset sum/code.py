656f1ad4bc7cf5739d748de9dc63a422a76d3aed
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-05 16:53:00 +0530

    Update code.py

--- gfg/Not a subset sum/code.py
+++ gfg/Not a subset sum/code.py
@@ -1,1 +1,11 @@
+#User function Template for python3
 
+class Solution:
+    def findSmallest(self, arr):
+        # code here
+        ans = 1
+        for n in arr:
+            if n > ans:
+                return ans
+            ans += n
+        return ans
