63744f60b2b2a3fd6955ccfe999c498d52fe0fb0
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-06 10:06:30 +0530

    Update code.py

--- gfg/Find the number of islands/code.py
+++ gfg/Find the number of islands/code.py
@@ -1,1 +1,31 @@
-
+class Solution:
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+        
+        n, m = len(grid), len(grid[0])
+        
+        def dfs(i, j):
+            # Check for out of bounds and if the cell is water
+            if i < 0 or i >= n or j < 0 or j >= m or grid[i][j] == 0:
+                return
+            
+            # Mark the cell as visited by sinking the island
+            grid[i][j] = 0
+            
+            # Explore all eight possible directions
+            directions = [
+                (1, 0), (0, 1), (-1, 0), (0, -1),
+                (1, 1), (-1, -1), (1, -1), (-1, 1)
+            ]
+            for di, dj in directions:
+                dfs(i + di, j + dj)
+        
+        island_count = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:  # Found an island
+                    island_count += 1
+                    dfs(i, j)  # Sink the island
+        
+        return island_count
