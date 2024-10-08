37781032b474380cfa986fc9417ec5cda3ae3160
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-08 20:16:33 +0530

    Update code.py

--- gfg/Largest Pair Sum/code.py
+++ gfg/Largest Pair Sum/code.py
@@ -1,1 +1,21 @@
 
+from typing import List
+
+
+class Solution:
+    def pairsum(self, arr : List[int]) -> int:
+        # code here
+        x,y = arr[0],arr[1]
+        for i in range(2,len(arr)):
+            if arr[i] > x:
+                t = x
+                x = arr[i]
+                if t > y:
+                    y = t
+                    
+            elif  arr[i] > y:
+                t = y 
+                y = arr[i]
+                if t > x:
+                    x = y
+        return x+y
