26535518b4159d8af0e849e668f45641ba34cfca
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-08 20:28:36 +0530

    Update code.py

--- lc/1963. Minimum Number of Swaps to Make the String Balanced/code.py
+++ lc/1963. Minimum Number of Swaps to Make the String Balanced/code.py
@@ -1,1 +1,14 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        depth,swaps = 0,0
+        for c in s:
+            if c == "[":
+                depth += 1
+            elif c == "]":
+                if depth > 0 :
+                    depth -= 1
+                else:
+                    depth += 1
+                    swaps += 1
+        return swaps
 
