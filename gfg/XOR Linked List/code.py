498580e02c9512f19ab826a601a9628234bba334
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-08 20:17:11 +0530

    Update code.py

--- gfg/XOR Linked List/code.py
+++ gfg/XOR Linked List/code.py
@@ -1,1 +1,24 @@
+def insert(head,data):
+    new_node = Node(data)
+    new_node.npx = id(head)  # npx of new node is current head's address
+        
+    if head is not None:
+        # Update npx of current head
+        head.npx = XOR(new_node, head.npx)
 
+    return new_node  # Move head to point to new node
+
+def getList(head):
+    result_forward = []
+    
+    current = head
+    prev_id = 0
+        
+    # Traverse forward
+    while current is not None:
+        result_forward.append(current.data)
+        next_id = XOR(prev_id, current.npx)
+        prev_id = id(current)
+        current = None if next_id == 0 else next_id
+        
+    return result_forward
