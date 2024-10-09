986221a02d3644c2274a4f9fbfc3f786e6455dd7
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-09 10:24:36 +0530

    Update code.py

--- lc/921. Minimum Add to Make Parentheses Valid/code.py
+++ lc/921. Minimum Add to Make Parentheses Valid/code.py
@@ -1,1 +1,16 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        ans = 0
+        open_count = 0
+        for c in s:
+            if c == "(":
+                open_count += 1
+            else:
+                open_count -= 1
+                if open_count < 0:
+                    open_count = 0
+                    ans += 1
 
+        return open_count + ans
+
+        
