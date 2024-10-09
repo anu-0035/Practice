2fcb3ec2badd34eba2a51eb05277abf63d787f13
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-09 10:31:49 +0530

    Update code.py

--- gfg/Linked List Matrix/code.py
+++ gfg/Linked List Matrix/code.py
@@ -1,1 +1,32 @@
+#User function Template for python3
 
+'''
+
+class Node():
+    def __init__(self,x):
+        self.data = x
+        self.right = None
+        self.down=None
+
+'''
+
+class Solution:
+    def constructLinkedMatrix(self, mat):
+        #your code goes here
+        if not mat: return none
+        
+        n = len(mat)
+        nodes = [[None for _ in range(n)] for _ in range(n)]
+        
+        for i in range(n):
+            for j in range(n):
+                nodes[i][j] = Node(mat[i][j])
+        for i in range(n):
+            for j in range(n):
+                if j + 1 <  n:
+                    nodes[i][j].right = nodes[i][j+1]
+                if i + 1 <  n:
+                    nodes[i][j].down = nodes[i+1][j]
+
+        return nodes[0][0]
+        
