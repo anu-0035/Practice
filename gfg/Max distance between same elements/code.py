f71cea36f45150eb7fe8965c712cf23a1915669b
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-10 09:24:39 +0530

    Update code.py

--- gfg/Max distance between same elements/code.py
+++ gfg/Max distance between same elements/code.py
@@ -1,1 +1,15 @@
+class Solution:
+    # Your task is to Complete this function
+    # functtion should return an integer
+    def maxDistance(self, arr):
+        # Code here
+        first_occurance = {}
+        max_dist = 0
+        for i in range(len(arr)):
+            if arr[i] not in first_occurance:
+                first_occurance[arr[i]] = i
+            else:
+                dist = i -  first_occurance[arr[i]]
+                max_dist = max(max_dist,dist)
+        return max_dist
 
