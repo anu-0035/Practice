862e54fc8913e305581672a734908d86ad1a2cce
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-10 09:36:11 +0530

    Update code.py

--- lc/962. Maximum Width Ramp/code.py
+++ lc/962. Maximum Width Ramp/code.py
@@ -1,1 +1,25 @@
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        indexed_num = [(v,i) for i,v in enumerate(nums) ]
+        indexed_num.sort()
+        max_width = 0
+        min_index = float("inf")
 
+        for v,i in indexed_num:
+            min_index = min(i , min_index)
+            max_width = max(max_width,i - min_index)
+        
+        return max_width
+    
+    # def maxWidthRamp(self, nums: List[int]) -> int:
+    #     stack = []
+    #     ans = 0
+    #     for i in range(len(nums)):
+    #         if not stack  or nums[stack[-1]] > nums[i]:
+    #             stack.append(i)
+
+    #     for i in range(len(nums))[::-1]:
+    #         while stack and nums[stack[-1]] <= nums[i]:
+    #             ans= max(ans,i -stack[-1])
+    #             stack.pop()
+    #     return ans
