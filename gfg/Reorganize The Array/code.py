b7f7227efeeecbd51da122b164bbd366b87c8773
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-11 09:19:11 +0530

    Update code.py

--- gfg/Reorganize The Array/code.py
+++ gfg/Reorganize The Array/code.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def rearrange(self, arr):
+        #Code here
+        n = len(arr)
+        ans = [-1] * n
+        for i in arr:
+            if i == -1:
+                continue
+            ans[i] = i
+        return ans
