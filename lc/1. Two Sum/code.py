fb6285d4aa3960d0841a7f738c9304b60b5efc00
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-12 12:07:25 +0530

    Update code.py

--- lc/1. Two Sum/code.py
+++ lc/1. Two Sum/code.py
@@ -1,1 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        required_index = {}
+        for i,n in enumerate(nums):
+            complement = target - n
+            if complement in required_index:
+                return [required_index[complement],i] 
+            required_index[n] = i
 
+        
+        
