2656e72d6dd58f7e02eeca93eba8ed565b69b55c
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-12 10:57:56 +0530

    Update code.py

--- gfg/Two Smallests in Every Subarray/code.py
+++ gfg/Two Smallests in Every Subarray/code.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def pairWithMaxSum(self, arr):
+        #code here
+        n = len(arr)
+        if n <= 1:
+            return -1
+        maxsum = -1
+        
+        for i in range(n-1):
+            first = min(arr[i],arr[i+1])
+            second = max(arr[i],arr[i+1])
+            maxsum = max(maxsum,first+second)
+        return maxsum
