8fc0f0ad627c9a1f70b366cb9fbfa3050d763856
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-13 08:37:31 +0530

    Update code.py

--- gfg/Delete Alternate Nodes/code.py
+++ gfg/Delete Alternate Nodes/code.py
@@ -1,1 +1,18 @@
+#User function Template for python3
 
+'''class Node: 
+   
+    # Function to initialize the node object 
+    def __init__(self, data): 
+        self.data = data  
+        self.next = None'''
+
+class Solution:
+    def deleteAlt(self, head):
+        # code here
+        if not head: return None
+        cur = head
+        while cur and cur.next:
+            cur.next = cur.next.next
+            cur = cur.next
+        return head
