f509d9b52216c24bf8545fcd19801e2ff17ce960
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-13 09:51:14 +0530

    Update code.py

--- lc/9. Palindrome Number/code.py
+++ lc/9. Palindrome Number/code.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # x = str(x)
+        # return str(x) == str(x)[::-1]
+        if x < 0:
+            return False
+        a,t = 0,x
+        while t:
+            a = a*10 + t % 10
+            t //= 10
+        return a == x
+        
