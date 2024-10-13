f2b8d20436301c52bf654743c9ce2aa6877783c8
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-13 09:46:44 +0530

    Update code.py

--- lc/7. Reverse Integer/code.py
+++ lc/7. Reverse Integer/code.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def reverse(self, x: int) -> int:
+        # rev = int(str(abs(x))[::-1])
+        # return (-rev if x < 0 else rev) if rev.bit_length() < 32 else 0
+        sign = 1
+        if x < 0:
+            sign = -1
+            x = -x
+        a = 0
+        while x:
+            a = a*10 + x % 10 
+            x //= 10
+        if a < -2**31   or a > 2**31 -1:
+            return 0
+        return a * sign
+        
+        
+        
