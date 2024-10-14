fa395cf5168db3f16862c27de75c91689182f3bb
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-14 14:45:26 +0530

    Update code.py

--- lc/12. Integer to Roman/code.py
+++ lc/12. Integer to Roman/code.py
@@ -1,1 +1,24 @@
-
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        value_map = [
+            (1000, 'M'),
+            (900, 'CM'),
+            (500, 'D'),
+            (400, 'CD'),
+            (100, 'C'),
+            (90, 'XC'),
+            (50, 'L'),
+            (40, 'XL'),
+            (10, 'X'),
+            (9, 'IX'),
+            (5, 'V'),
+            (4, 'IV'),
+            (1, 'I')
+            ]
+        roman_numeral = ""
+        for val,symbol in value_map:
+            while num >= val:
+                num -= val
+                roman_numeral += symbol
+        return roman_numeral
+        
