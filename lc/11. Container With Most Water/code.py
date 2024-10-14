11167d7b82c62b8fd47887302b90a0241575ddaa
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-14 14:42:07 +0530

    Update code.py

--- lc/11. Container With Most Water/code.py
+++ lc/11. Container With Most Water/code.py
@@ -1,1 +1,18 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left,right = 0 ,len(height) - 1
+        max_area = 0
 
+        while left < right:
+            cur_height = min(height[left],height[right])
+            width = right - left
+            cur_area = width * cur_height
+            max_area = max(max_area,cur_area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+        
