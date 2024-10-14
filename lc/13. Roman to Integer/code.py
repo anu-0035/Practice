e0285eed88e789de01fef832d838b8cf3ad2b48c
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-14 14:47:08 +0530

    Update code.py

--- lc/13. Roman to Integer/code.py
+++ lc/13. Roman to Integer/code.py
@@ -1,1 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total = 0 
+        n = len(s)
 
+        for i in range(n):
+            if i < n-1 and roman_map[s[i]] < roman_map[s[i+1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+        return total
+        
