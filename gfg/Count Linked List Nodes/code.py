0f72d10679f755a7ceee23b868cdf948fbf03859
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-14 08:35:05 +0530

    Update code.py

--- gfg/Count Linked List Nodes/code.py
+++ gfg/Count Linked List Nodes/code.py
@@ -1,1 +1,21 @@
 
+#User function Template for python3
+
+'''
+
+#Linked list class
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        '''
+class Solution:
+    # Function to count nodes of a linked list.
+    def getCount(self, head):
+        # code here
+        ans = 0 
+        t = head
+        while t:
+            ans += 1
+            t = t.next
+        return ans
