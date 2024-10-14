bcab7ab2cc230a634418c81b762b1dac8d7b3466
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-14 08:44:13 +0530

    Update code.py

--- lc/2530. Maximal Score After Applying K Operations/code.py
+++ lc/2530. Maximal Score After Applying K Operations/code.py
@@ -1,1 +1,12 @@
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        max_heap = [-n for n in nums]
+        heapq.heapify(max_heap)
+        total_score = 0 
+        for _ in range(k):
+            max_val = -heapq.heappop(max_heap)
+            total_score += max_val
+            heapq.heappush(max_heap,-math.ceil(max_val/3))
+        return total_score
 
+        
