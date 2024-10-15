a4d99fc34a8a122afec99a474f1ace93c883ca3e
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-15 08:22:54 +0530

    Update code.py

--- lc/2938. Separate Black and White Balls/code.py
+++ lc/2938. Separate Black and White Balls/code.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        # l,ans = 0,0
+        # for r in range(len(s)):
+        #     if s[r] == '0':
+        #         ans += (r-l)
+        #         l += 1
+        # return ans
+        ans, swaps = 0, 0
+        for c in s:
+            if c == '1':
+                swaps += 1
+            else:
+                ans += swaps
+        return ans
+         
