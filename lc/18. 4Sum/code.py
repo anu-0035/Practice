b63d7bb619c4b170d98a83da3648ec7b235c163c
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-17 10:33:02 +0530

    Update code.py

--- lc/18. 4Sum/code.py
+++ lc/18. 4Sum/code.py
@@ -1,1 +1,30 @@
-
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        n = len(nums)
+        nums.sort()
+        ans = []
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            for j in range(i+1,n):
+                if j > i+1  and nums[j] == nums[j-1]:
+                    continue
+                
+                l,r = j+1 ,n-1
+                while  l < r:
+                    summ = nums[i] + nums[j] + nums[l] + nums[r]
+                    if summ == target:
+                        ans.append([nums[i] , nums[j] , nums[l] , nums[r]])
+                        l,r = l+1,r-1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r+1]:
+                            r -= 1
+                    elif summ > target:
+                        r -= 1
+                    else:
+                        l += 1
+        return ans
+    
+        
