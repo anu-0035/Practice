0d15105440a2ca9183aadcf97d6c5b5cac412505
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-17 10:35:55 +0530

    Update code.py

--- lc/20. Valid Parentheses/code.py
+++ lc/20. Valid Parentheses/code.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c in "[{(":
+                stack.append(c)
+            else:
+                if c == ")" and stack and stack[-1] == "(":
+                    stack.pop()
+                elif c == "]" and stack and stack[-1] == "[":
+                    stack.pop()
+                elif c == "}" and stack and stack[-1] == "{":
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
+        
