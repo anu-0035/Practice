2e87e4fea478f5b0bf0457cbed679eea9ec5f14e
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-17 10:03:22 +0530

    Update code.py

--- gfg/Two Swaps/code.py
+++ gfg/Two Swaps/code.py
@@ -1,1 +1,17 @@
+class Solution:
+    def checkSorted(self, arr):
+        #code here
+        n = len(arr)
+        swapCnt = 0
 
+        for i in range(n):
+            if arr[i] == i + 1:
+                continue
+            
+            while arr[i] != i + 1:
+                arr[arr[i] - 1], arr[i] = arr[i], arr[arr[i] - 1]
+                swapCnt += 1
+
+        if swapCnt == 2 or swapCnt == 0:
+            return True
+        return False
