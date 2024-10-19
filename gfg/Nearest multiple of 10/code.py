8b7623c4eb9444b2d9b2eba7cf1298ffe37f1de9
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-19 21:04:04 +0530

    Update code.py

--- gfg/Nearest multiple of 10/code.py
+++ gfg/Nearest multiple of 10/code.py
@@ -1,1 +1,16 @@
 
+class Solution:
+    def roundToNearest (self, s) : 
+        #Complete the function
+        last = s[-1]
+        if last <= "5":
+            return s[:-1]+"0"
+        s = s[:-1] + "0"
+        n = len(s)
+        i = -2
+        while i>= -n and s[i] =="9":
+            s = s[:i] + "0" + s[i+1:]
+            i -= 1
+        if i < -n:
+            return "1" + s
+        return s[:i] + str(int(s[i])+1)+s[i+1:]
