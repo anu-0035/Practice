8269b95c17e1c2bdd6f34f4bdb4fad5c6069f236
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-20 09:29:07 +0530

    Update code.py

--- gfg/Sort a k sorted doubly linked list/code.py
+++ gfg/Sort a k sorted doubly linked list/code.py
@@ -1,1 +1,33 @@
-
+#User function Template for python3
+'''
+class DLLNode:
+    def __init__(self,val) -> None:
+        self.data = val
+        self.prev = None
+        self.next = None
+'''
+class Solution:
+    def sortAKSortedDLL(self, head, k):
+        # Code Here
+        if not head : return None
+        heap = []
+        cur = head
+        for  _ in range(k+1):
+            heapq.heappush(heap,cur.data)
+            cur = cur.next
+        
+        sorted_head = DLLNode(0)
+        last = sorted_head
+        
+        while heap:
+            smallest = DLLNode(heapq.heappop(heap))
+            last.next = smallest
+            smallest.prev = last
+            last = smallest
+            
+            if cur:
+                heapq.heappush(heap,cur.data)
+                cur = cur.next
+        last.next = None
+        return sorted_head.next
+            
