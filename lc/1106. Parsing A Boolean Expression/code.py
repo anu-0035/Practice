fc9740c83c3c4390399a439df9583895b8096c35
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-20 09:09:31 +0530

    Update code.py

--- lc/1106. Parsing A Boolean Expression/code.py
+++ lc/1106. Parsing A Boolean Expression/code.py
@@ -1,1 +1,30 @@
-
+class Solution:
+    def parseBoolExpr(self, expression: str) -> bool:
+        s = expression
+        i = 0
+        def helper():
+            nonlocal i
+            c = s[i]
+            i += 1
+            if c == "t": return True
+            if c == "f": return False
+            if c == "!":
+                i += 1
+                res = not helper()
+                i += 1
+                return res
+            children = []
+            i += 1
+            while s[i] != ")":
+                if s[i] != ",":
+                    children.append(helper())
+                else:
+                    i += 1
+            i += 1
+            res = children[0]
+            if c == "&":
+                return all(children)
+            if c == "|":
+                return any(children)
+        return helper()
+        
