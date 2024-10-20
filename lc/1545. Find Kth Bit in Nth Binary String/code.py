ff55f4e606d9f146ba58c7bb2979712d45e2f9bd
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-20 09:11:12 +0530

    Update code.py

--- lc/1545. Find Kth Bit in Nth Binary String/code.py
+++ lc/1545. Find Kth Bit in Nth Binary String/code.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        length = 2**n -1
+        def helper(length,k):
+            if length == 1:
+                return "0"
+            half = length//2
+            if half >= k:
+                return helper(half,k)
+            elif half+1 < k:
+                res = helper(half,1+length-k)
+                return "0" if res == "1" else "1"
+            else:
+                return "1"
+        return helper(length,k)
