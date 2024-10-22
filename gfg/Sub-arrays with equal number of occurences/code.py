12605f671947853315d6f289e38cf81eb949cac4
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-22 12:22:59 +0530

    Update code.py

--- gfg/Sub-arrays with equal number of occurences/code.py
+++ gfg/Sub-arrays with equal number of occurences/code.py
@@ -1,1 +1,18 @@
-
+#User function Template for python3
+from collections import defaultdict
+class Solution:
+    def sameOccurrence(self, arr, x, y):
+        # code here
+        diff_count = defaultdict(int)
+        diff_count[0] = 1
+        diff = 0
+        count = 0
+        
+        for n in arr:
+            if n == x:
+                diff += 1
+            elif n == y:
+                diff -= 1
+            count += diff_count[diff] 
+            diff_count[diff] += 1
+        return count
