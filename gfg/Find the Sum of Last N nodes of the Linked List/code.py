6b2526e5d6c870ce55fc67b4c54b55145b6d1377
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-23 20:36:18 +0530

    Update code.py

--- gfg/Find the Sum of Last N nodes of the Linked List/code.py
+++ gfg/Find the Sum of Last N nodes of the Linked List/code.py
@@ -1,1 +1,26 @@
+#User function Template for python3
 
+'''
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def sumOfLastN_Nodes(self, head, n):
+        #function should return sum of last n nodes
+        dummy = Node(0)
+        dummy.next = head
+        fast = slow = dummy
+        for i in range(n):
+            fast = fast.next
+        s = 0
+        while fast:
+            slow = slow.next
+            fast = fast.next
+        
+        while slow:
+            s += slow.data
+            slow = slow.next
+        return s
