39b7bae349f02c00fa9e45cbdd1a212514c1e31f
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-27 08:55:12 +0530

    Update code.py

--- gfg/Alternative Sorting/code.py
+++ gfg/Alternative Sorting/code.py
@@ -1,1 +1,14 @@
+class Solution:
+    def alternateSort(self,arr):
+        # Your code goes here
+        arr.sort()
+        n = len(arr)
+        a,b = arr[:n//2],arr[n//2:] 
+        ans = []
+        for i in range(n//2):
+            ans.append(b.pop())
+            ans.append(a[i])
+        if b:
+            ans.append(b.pop())
+        return ans
 
