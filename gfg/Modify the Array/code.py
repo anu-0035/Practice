5217223cfd0dc566ed2c19cca1f31c4b2579e40b
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-27 08:53:08 +0530

    Update code.py

--- gfg/Modify the Array/code.py
+++ gfg/Modify the Array/code.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def modifyAndRearrangeArr (self, arr) : 
+        #Complete the function
+        n = len(arr)
+        i = 0
+        while i < n-1:
+            if arr[i] != 0 and arr[i] == arr[i+1]:
+                arr[i] *= 2
+                arr[i+1] = 0
+                i += 1
+            i += 1
+        ans = [n for n in arr if n != 0 ]
+        zeros = arr.count(0)
+        return ans + [0] * zeros
