8cb5e35808507268389cd3fee1d9cd13c7205007
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-10-27 08:58:53 +0530

    Update code.py

--- gfg/Triplet Family/code.py
+++ gfg/Triplet Family/code.py
@@ -1,1 +1,19 @@
+"""  
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+  This is method only submission.
+  You only need to complete the method.
+"""
+class Solution:
+    def count(self, head, key):
+        # Code here
+        count = 0
+        t = head
+        while t: 
+            if t.data == key:
+                count += 1
+            t = t.next
+        return count
 
