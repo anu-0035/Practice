f40438bd637008c8a498635e7036324afa74895d
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-06 20:38:49 +0530

    Update code.py

--- lc/3011. Find if Array Can Be Sorted/code.py
+++ lc/3011. Find if Array Can Be Sorted/code.py
@@ -1,1 +1,19 @@
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        n = len(nums)
+        values = nums.copy()
+        for i in range(n):
+            flag = False
+            for j in range(n-i-1):
+                if values[j] <= values[j+1]:
+                    continue
+                if bin(values[j]).count("1") == bin(values[j+1]).count("1"):
+                    values[j],values[j+1] = values[j+1],values[j]
+                    flag = True
+                else:
+                    return False
+            if not flag:
+                break
+        return True
 
+        
