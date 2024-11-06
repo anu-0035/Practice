e4c44731ad17e658c97cbcaec46a81b7043a6f3a
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-06 20:36:44 +0530

    Update code.py

--- gfg/Root to leaf paths sum/code.py
+++ gfg/Root to leaf paths sum/code.py
@@ -1,1 +1,13 @@
-
+# Your task is to complete this function
+# function should return max sum level in the tree
+class Solution:
+    def treePathSum(self,root):
+        # Code here
+        return self.dfs(root,0)
+    def dfs(self,node,cur_sum):
+        if not node:
+            return 0
+        cur_sum = cur_sum * 10 + node.data
+        if not node.left and not node.right:
+            return cur_sum
+        return self.dfs(node.left,cur_sum) + self.dfs(node.right,cur_sum)
