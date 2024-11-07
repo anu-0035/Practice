aef29cbe303c80294225700efec32cbb560e3130
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-07 10:12:44 +0530

    Update code.py

--- lc/2275. Largest Combination With Bitwise AND Greater Than Zero/code.py
+++ lc/2275. Largest Combination With Bitwise AND Greater Than Zero/code.py
@@ -1,1 +1,11 @@
+class Solution:
+    def largestCombination(self, candidates: List[int]) -> int:
+        res = 0
+        for i in range(32):
+            cnt = 0
+            for n in candidates:
+                cnt += 1 if (1 << i) & n else 0
+            res = max(res,cnt)
+        return res
 
+        
