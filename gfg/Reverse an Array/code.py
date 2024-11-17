bd311fcf68052873e212ac9d93d53d46f657641e
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-17 09:32:56 +0530

    Update code.py

--- gfg/Reverse an Array/code.py
+++ gfg/Reverse an Array/code.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        l,r = 0,len(arr)-1
+        
+        while l < r:
+            arr[l],arr[r] = arr[r],arr[l]
+            l += 1
+            r -= 1
+        
