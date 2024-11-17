3fb721a96f4adef814a0daa8bef0f3976c4c54b7
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-17 10:02:57 +0530

    Update code.py

--- lc/862. Shortest Subarray with Sum at Least K/code.py
+++ lc/862. Shortest Subarray with Sum at Least K/code.py
@@ -1,1 +1,20 @@
-
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        q = deque()
+        res = float('inf')
+        cur_sum = 0
+        for r in range(len(nums)):
+            cur_sum += nums[r]
+            if cur_sum >= k:
+                res = min(res,r+1)
+            
+            while q and cur_sum - q[0][0] >= k:
+                prefix,end_idx = q.popleft()
+                res = min(res,r - end_idx)
+            
+            while q and q[-1][0] > cur_sum:
+                q.pop()
+            q.append((cur_sum,r))
+        
+        return -1 if res == float("inf") else res
+        
