61f9babdf3f64deb7bd24c37bfdb72af0f6d3c73
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-18 11:05:24 +0530

    Update code.py

--- gfg/Rotate Array/code.py
+++ gfg/Rotate Array/code.py
@@ -1,1 +1,16 @@
+#User function Template for python3
 
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self, arr, d):
+        #Your code here
+        n = len(arr)
+        d = d % n
+        def reverse(start,end):
+            while start < end:
+                arr[start],arr[end] = arr[end],arr[start]
+                start += 1
+                end -= 1
+        reverse(0,d - 1)
+        reverse(d,n-1)
+        reverse(0,n-1)
