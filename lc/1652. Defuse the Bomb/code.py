a6964c979887458019d9a636188d28431968650b
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-18 20:26:28 +0530

    Update code.py

--- lc/1652. Defuse the Bomb/code.py
+++ lc/1652. Defuse the Bomb/code.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
+        ans = [0] * n
+        l = 0
+        cur_sum = 0
+        for r in range(n + abs(n)):
+            cur_sum += code[r % n]
+            if r - l + 1 >  abs(k):
+                cur_sum -= code[l % n]
+                l = (l+1) % n
+            if r - l + 1 == abs(k):
+                if k > 0:
+                    ans[(l -1) % n] = cur_sum
+                elif k < 0:
+                    ans[(r+1) % n] = cur_sum
+        return ans
+        
