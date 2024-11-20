51fbf3c416495a89fe194b1157ce1112359f1cb9
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-20 21:39:56 +0530

    Update code.py

--- lc/2516. Take K of Each Character From Left and Right/code.py
+++ lc/2516. Take K of Each Character From Left and Right/code.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        n = len(s)
+        count = [0,0,0]
+        for c in s:
+            count[ord(c) - 97] += 1
+        if min(count) < k:
+            return -1
+        
+        res = float("inf")
+        l = 0
+        for r in range(n):
+            count[ord(s[r]) - 97] -= 1
+            while min(count) < k:
+                count[ord(s[l]) - 97] += 1
+                l += 1
+            res = min(res,n - (r - l + 1))
+        return res
+        
