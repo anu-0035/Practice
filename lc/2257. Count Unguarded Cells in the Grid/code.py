44e522a7abca3aebf4def85507d15e7b4f77ff7f
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-21 21:41:34 +0530

    Update code.py

--- lc/2257. Count Unguarded Cells in the Grid/code.py
+++ lc/2257. Count Unguarded Cells in the Grid/code.py
@@ -1,1 +1,36 @@
+class Solution:
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        grid = [[0] * n for i in range(m)]
+        # 0 -> free, 1 -> guard, 2 -> wall, 3 -> gurdable
+        for r,c in guards:
+            grid[r][c] = 1
+        for r,c in walls:
+            grid[r][c] = 2
+        def mark(r,c):
+            for row in range(r + 1,m):
+                if grid[row][c] in [1,2]:
+                    break
+                grid[row][c] = 3
+            for row in reversed(range(0,r)):
+                if grid[row][c] in [1,2]:
+                    break
+                grid[row][c] = 3
+            for col in range(c + 1,n):
+                if grid[r][col] in [1,2]:
+                    break
+                grid[r][col] = 3
+            for col in reversed(range(0,c)):
+                if grid[r][col] in [1,2]:
+                    break
+                grid[r][col] = 3
 
+        for r,c in guards:
+            mark(r,c)
+        ans = 0
+        for row in grid:
+            for r in row:
+                if r == 0:
+                    ans += 1
+        return ans
+
+        
