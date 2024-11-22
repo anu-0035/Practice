f26df0899c3affa5e3c05f6cad08a47b406fe832
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-22 20:48:06 +0530

    Update code.py

--- gfg/Stock Buy and Sell – Max one Transaction Allowed/code.py
+++ gfg/Stock Buy and Sell – Max one Transaction Allowed/code.py
@@ -1,1 +1,13 @@
+class Solution:
+    def maximumProfit(self, prices):
+        # code here
+        min_so_far = float("inf")
+        max_profit = 0
+        
+        for p in prices:
+            min_so_far = min(p,min_so_far)
+            
+            max_profit = max(max_profit,p -min_so_far)
 
+        return max_profit
+        
