65c6488ad26c94616b0cf4b6e61fb74afae9fc9f
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-23 17:15:37 +0530

    Update code.py

--- gfg/Stock Buy and Sell – Multiple Transaction Allowed/code.py
+++ gfg/Stock Buy and Sell – Multiple Transaction Allowed/code.py
@@ -1,1 +1,14 @@
+from typing import List
 
+
+class Solution:
+    def maximumProfit(self, prices) -> int:
+        # code here
+        n = len(prices)
+        if n <= 1:
+            return 0
+        max_profit = 0
+        for i in range(1,n):
+            if prices[i] > prices[i-1]:
+                max_profit += prices[i] - prices[i-1]
+        return max_profit
