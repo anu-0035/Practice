16e4cb347cf6f832ce7fdc9079a5e78f35473632
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-24 10:02:02 +0530

    Update code.py

--- lc/1975. Maximum Matrix Sum/code.py
+++ lc/1975. Maximum Matrix Sum/code.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        n = len(matrix)
+        count,mini = 0,float("inf")
+        res = 0
+        for i in range(n):
+            for j in range(n):
+                x = abs(matrix[i][j])
+                res += x
+                mini = min(mini,x)
+                if matrix[i][j] <= 0:
+                    count += 1
+        if count % 2 == 0:
+            return res
+        return res - 2 * mini
+        
