9299056f0dcfcf99094a434cefe122c1acf7eb86
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-27 19:58:50 +0530

    Update code.py

--- gfg/Smallest Positive Missing Number/code.py
+++ gfg/Smallest Positive Missing Number/code.py
@@ -1,1 +1,18 @@
+#User function Template for python3
 
+class Solution:
+    
+    #Function to find the smallest positive number missing from the array.
+    def missingNumber(self,arr):
+        #Your code here
+        n = len(arr)
+        
+        
+        for i in range(n):
+            while 1 <= arr[i] <= n and arr[arr[i] - 1] != arr[i]:
+                arr[arr[i] -1 ],arr[i] = arr[i],arr[arr[i]-1]
+        for i in range(n):
+            if arr[i] != i+1:
+                return i+1
+        return n+1
+        
