f16b62f849689c0155e8c2088fec6ad050474589
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-29 11:20:08 +0530

    Update code.py

--- gfg/Add Binary Strings/code.py
+++ gfg/Add Binary Strings/code.py
@@ -1,1 +1,19 @@
-
+#User function Template for python3
+# class Solution:
+#     def num(self,s):
+#         s = s[::-1]
+#         x = 0
+#         for i in range(len(s)):
+#             if  s[i] == "1":
+#                 x += (2 ** i)
+#         return x
+    
+# 	def addBinary(self, s1, s2):
+# 		# code here
+# 		s1 = self.num(s1)
+# 		s2 = self.num(s2)
+# 		return bin(s1+s2)[2:]
+class Solution:
+	def addBinary(self, s1, s2):
+		# code here
+		return bin(int(s1, 2) + int(s2, 2))[2:]
