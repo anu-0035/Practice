401479bd4fd2a978a99746dafa03c80ffbaf88f7
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-30 11:05:26 +0530

    Update code.py

--- lc/2577. Minimum Time to Visit a Cell In a Grid/code.py
+++ lc/2577. Minimum Time to Visit a Cell In a Grid/code.py
@@ -1,1 +1,22 @@
-
+class Solution:
+    def minimumTime(self, grid: List[List[int]]) -> int:
+        if min(grid[0][1], grid[1][0] ) > 1:
+            return -1
+        min_heap = [(0,0,0)]
+        ROWS,COLS = len(grid),len(grid[0])
+        visit = set()
+        while min_heap:
+            t,r,c = heapq.heappop(min_heap)
+            if (r,c) == (ROWS -1,COLS -1):
+                return t
+            nei = [[r+1,c],[r-1,c],[r,c+1],[r,c-1]]
+            for nr,nc in nei:
+                if (nr,nc) in visit or nr < 0 or nc < 0 or nr == ROWS or nc == COLS:
+                    continue
+                wait = 0
+                if abs(grid[nr][nc] - t) % 2 == 0:
+                    wait = 1
+                n_time = max(grid[nr][nc] + wait,t+1)
+                heapq.heappush(min_heap,(n_time,nr,nc))
+                visit.add((nr,nc))
+        
