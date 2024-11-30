f551d2de41c5cda778d83a6836ea6da15c84380b
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-11-30 10:39:16 +0530

    Update code.py

--- gfg/Anagram/code.py
+++ gfg/Anagram/code.py
@@ -1,1 +1,18 @@
+#User function Template for python3
+from collections import defaultdict
 
+class Solution:
+    
+    #Function is to check whether two strings are anagram of each other or not.
+    def areAnagrams(self, s1, s2):
+        #code here
+        if len(s1) != len(s2):
+            return False
+        freq = defaultdict(int)
+        for c in s1:
+            freq[c] += 1
+        for c in s2:
+            if freq[c] == 0:
+                return False
+            freq[c] -= 1
+        return True
