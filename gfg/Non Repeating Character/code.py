5dc4fce9452f8bd80f4d69ad64387a5da54c1853
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-12-01 14:06:51 +0530

    Update code.py

--- gfg/Non Repeating Character/code.py
+++ gfg/Non Repeating Character/code.py
@@ -1,1 +1,14 @@
-
+#User function Template for python3
+from collections import defaultdict
+class Solution:
+    
+    #Function to find the first non-repeating character in a string.
+    def nonRepeatingChar(self,s):
+        #code here
+        freq = defaultdict(int)
+        for c in s:
+            freq[c] += 1
+        for c in s:
+            if freq[c] == 1:
+                return c
+        return -1
