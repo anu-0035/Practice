81d02fbdf0cb93629056cc18a8bed72cac7fbb3b
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-12-02 11:29:26 +0530

    Update code.py

--- gfg/Search Pattern (KMP-Algorithm)/code.py
+++ gfg/Search Pattern (KMP-Algorithm)/code.py
@@ -1,1 +1,39 @@
+class Solution:
+    def search(self, pat: str, txt: str) -> list:
+        def compute_lps(pattern):
+            lps = [0] * len(pattern)
+            length = 0
+            i = 1
+            
+            while i < len(pattern):
+                if pattern[i] == pattern[length]:
+                    length += 1
+                    lps[i] = length
+                    i += 1
+                else:
+                    if length != 0:
+                        length = lps[length - 1]
+                    else:
+                        lps[i] = 0
+                        i += 1
+            return lps
 
+        lps = compute_lps(pat)
+        indices = []
+        i = j = 0
+        
+        while i < len(txt):
+            if pat[j] == txt[i]:
+                i += 1
+                j += 1
+            
+            if j == len(pat):
+                indices.append(i - j)
+                j = lps[j - 1]
+            elif i < len(txt) and pat[j] != txt[i]:
+                if j != 0:
+                    j = lps[j - 1]
+                else:
+                    i += 1
+
+        return indices
