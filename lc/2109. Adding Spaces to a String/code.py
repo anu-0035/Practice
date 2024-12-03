94350583d02b30cf60569a0b9dec03ac67ab6042
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-12-03 11:13:17 +0530

    Update code.py

--- lc/2109. Adding Spaces to a String/code.py
+++ lc/2109. Adding Spaces to a String/code.py
@@ -1,1 +1,10 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        index, res = 0, []
 
+        for space in spaces:
+            res.append(s[index : space])
+            index = space
+        
+        res.append(s[index :])
+        return " ".join(res)
