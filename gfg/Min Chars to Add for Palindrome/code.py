cb611affc5c548d0410b6aacb7ea3484b9012c40
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2024-12-03 11:11:59 +0530

    Update code.py

--- gfg/Min Chars to Add for Palindrome/code.py
+++ gfg/Min Chars to Add for Palindrome/code.py
@@ -1,1 +1,24 @@
-
+class Solution:
+    def minChar(self, s):
+        #Write your code here
+        def LPS(s):
+            lps = [0] * len(s)
+            length = 0
+            i = 1
+            while i < len(s):
+                if s[i] == s[length]:
+                    length += 1
+                    lps[i] = length
+                    i += 1
+                else:
+                    if length != 0:
+                        length = lps[length - 1]
+                    else:
+                        lps[i] = 0
+                        i += 1
+            return lps[-1]
+        
+        rev_s = s[::-1]
+        combined = s+ "#" + rev_s 
+        lps = LPS(combined)
+        return len(s) - lps
